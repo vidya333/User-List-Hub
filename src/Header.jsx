3c2a08c29dc97f205eb78ea2783eb52dd06bdefe
Vidya <vidya.nk07@gmail.com>
2024-07-04 20:35:49 +0530

    second commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 function Header() {
   return (
+    <>
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">
+            <span className="navbar-brand">
             <i className="fa-solid fa-user-group"></i>
-            </a>
+            </span>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-between">
                     <li className="nav-item">
-                        <a className="nav-link" href='/'>
+                        <span className="nav-link">
                             UserListHub
-                        </a>
+                        </span>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+    </>
   )
 }
 
